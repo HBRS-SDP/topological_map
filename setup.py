4b6699556397f70b7a5aeb26ae3864836805f92d
hwalli92 <62026003+hwalli92@users.noreply.github.com>
2021-01-21 09:19:39 +0100

    add packages line to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup
 
 setup(name='topological_map',
+      packages=['topological_map','topological_map.common.map_utils', 'topological_map.maps'],
       version='1.0.0',
       description='BRSU Topological Map',
       package_dir={'': '.'}
