cee2f3a848df7ffce195c23db2c0a93bdb68748a
hwalli92 <62026003+hwalli92@users.noreply.github.com>
2021-01-21 10:36:15 +0100

    add package_data to setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,10 @@ setup(
         "topological_map",
         "topological_map.common.map_utils",
         "topological_map.maps",
-        "topological_map.maps.brsu-full",
-        "topological_map.maps.brsu-large",
-        "topological_map.maps.brsu-osm",
-        "topological_map.maps.brsu-small",
-        "topological_map.maps.brsu-small-free",
-        "topological_map.maps.brsu-small-obs",
+        "topological_map.maps.*
     ],
     version="1.0.0",
     description="BRSU Topological Map",
     package_dir={"": "."},
+    package_data={'': ['*.yaml', '*.pgm']},
 )
