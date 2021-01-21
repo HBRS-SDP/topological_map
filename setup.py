2f52d6c532b9f67c14a0382409005b594f101c95
hwalli92 <62026003+hwalli92@users.noreply.github.com>
2021-01-21 10:40:28 +0100

    fix syntax error in setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
         "topological_map",
         "topological_map.common.map_utils",
         "topological_map.maps",
-        "topological_map.maps.*
+        "topological_map.maps.*",
     ],
     version="1.0.0",
     description="BRSU Topological Map",
     package_dir={"": "."},
-    package_data={'': ['*.yaml', '*.pgm']},
+    package_data={"": ["*.yaml", "*.pgm"]},
 )
