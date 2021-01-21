d0b980020f26eb817b26af3e2f6a20e5a42cd15f
hwalli92 <62026003+hwalli92@users.noreply.github.com>
2021-01-21 09:26:35 +0100

    add each map to packages in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,20 @@
 
 from setuptools import setup
 
-setup(name='topological_map',
-      packages=['topological_map','topological_map.common.map_utils', 'topological_map.maps'],
-      version='1.0.0',
-      description='BRSU Topological Map',
-      package_dir={'': '.'}
-      )
+setup(
+    name="topological_map",
+    packages=[
+        "topological_map",
+        "topological_map.common.map_utils",
+        "topological_map.maps",
+        "topological_map.maps.brsu-full",
+        "topological_map.maps.brsu-large",
+        "topological_map.maps.brsu-osm",
+        "topological_map.maps.brsu-small",
+        "topological_map.maps.brsu-small-free",
+        "topological_map.maps.brsu-small-obs",
+    ],
+    version="1.0.0",
+    description="BRSU Topological Map",
+    package_dir={"": "."},
+)
